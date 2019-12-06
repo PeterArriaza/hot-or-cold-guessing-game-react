c3e8e1eb0b0620ac47c40709e533de1e4994e060
PeterArriaza <peter.arriaza@gmail.com>
2019-12-05 21:20:54 -0500

    fixed merge issues of duplicate files

--- src/store.js
+++ src/store.js
@@ -1,11 +1,5 @@
 import { createStore } from "redux";
 
-<<<<<<< HEAD
 import reducer from "./reducer";
 
 export default createStore(reducer);
-=======
-import { gameReducer } from "./reducers/reducers";
-
-export default createStore(gameReducer);
->>>>>>> master
